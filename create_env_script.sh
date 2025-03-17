92fc4b1359aad1e7e8957fdab3a049ced799b4d8
lorenzo <lorenzo.cerrone@uzh.ch>
2025-03-17 12:30:58 +0100

    bump version

--- create_env_script.sh
+++ create_env_script.sh
@@ -1,4 +1,4 @@
-VERSION="0.1.1"
+VERSION="0.2.0"
 COMMMAND="mamba"
 # If ENVPREFIX is not NULL, the environment will be created with the prefix $ENVPREFIX/$ENVNAME 
 # If ENVPREFIX is NULL, the environment will be created in the default location
