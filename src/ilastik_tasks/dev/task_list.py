971cd28bf65e47d040cb440da44d7daef85396aa
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-06-24 10:22:41 +0200

    Update `dev` folder

--- src/ilastik_tasks/dev/task_list.py
+++ src/ilastik_tasks/dev/task_list.py
@@ -1,6 +1,9 @@
 """Contains the list of tasks available to fractal."""
 
-from fractal_tasks_core.dev.task_models import ParallelTask
+from fractal_task_tools.task_models import ParallelTask
+
+AUTHORS = "Lorenzo Cerrone"
+DOCS_LINK = "https://github.com/fractal-analytics-platform/fractal-ilastik-tasks"
 
 TASK_LIST = [
     ParallelTask(
